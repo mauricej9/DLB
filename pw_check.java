8c18964d208c64930182a06ca939b37382bd4947
Maurice Jenkins <mauricejenkins9@gmail.com>
2015-09-30 10:27:10 -0400

    1

--- pw_check.java
+++ pw_check.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+
+
 
 public class pw_check {
 
@@ -8,21 +11,27 @@ public class pw_check {
 		public DLBnode rightSib;
 		public DLBnode child;
 		
+		private DLBnode(char letter,  DLBnode sib,  DLBnode child) {
+            this.val = letter;
+            this.rightSib = sib;
+            this.child = child;
+        }
 		
-		public void buildTrie() {
+		
+		
+		
+	}
+	public static void buildTrie(DLBnode newDLB) throws IOException {
 			// TODO Auto-generated method stub
 			
 		}
-		
-	}
 	
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		  DLBnode newDLB = new DLBnode();
+		  DLBnode DLB = new DLBnode();
 		if (args.length>0){
 			if (args[0].equals("-g")){
-				newDLB.buildTrie();
+				buildTrie(DLB);
 			}
 		}
 	}
