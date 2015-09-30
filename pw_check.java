2ce7ee6e2f62d4b6af83e15efe913994d8b2067a
Maurice Jenkins <mauricejenkins9@gmail.com>
2015-09-30 17:56:09 -0400

    can now build trie

--- pw_check.java
+++ pw_check.java
@@ -8,11 +8,10 @@ public class pw_check {
 	static boolean empty = true;
 	private static DLBnode root;
 
-	
-	public pw_check(){
-		
+	public pw_check() {
+
 	}
-	
+
 	private static class DLBnode {
 		public char val;
 		public DLBnode rightSib;
@@ -28,34 +27,51 @@ public class pw_check {
 
 	public static void buildTrie(DLBnode newDLB) throws IOException {
 		// TODO Auto-generated method stub
-		
-		/*BufferedReader read = new BufferedReader(new FileReader(
-				"dictionary.txt"));*/
-		
-		BufferedReader read = new BufferedReader(new FileReader("C:/Users/maurice/cs1501/DLB/src/small_dict.txt"));												//for testing purposes 
+
+		/*
+		 * BufferedReader read = new BufferedReader(new FileReader(
+		 * "dictionary.txt"));
+		 */
+		DLBnode rootNode = root;
+		BufferedReader read = new BufferedReader(new FileReader(
+				"C:/Users/maurice/cs1501/DLB/src/small_dict.txt")); // for
+																	// testing
+																	// purposes
 		String s;
-		File file = new File("C:/Users/maurice/cs1501/DLB/src/my_dictionary.txt");
+		File file = new File(
+				"C:/Users/maurice/cs1501/DLB/src/my_dictionary.txt");
 		file.createNewFile();
-	    FileWriter writer = new FileWriter(file); 
+		FileWriter writer = new FileWriter(file);
 		while ((s = read.readLine()) != null) {
 			if (s.length() < 6) {
-				writer.write(s+"\n");
-				writer.write(System.getProperty( "line.separator" ));
-				
-				s += "'"; 													// the character ' signifies the end of the string in the trie
+				writer.write(s + "\n");
+				writer.write(System.getProperty("line.separator"));
+
+				s += "'"; // the character ' signifies the end of the string in
+							// the trie
 				add(s);
 			}
 		}
-		//writer.flush();
-	    writer.close();
+		// writer.flush();
+		writer.close();
 		read.close();
-		printTrie(root, "");
-		
+		printTrie(rootNode, "");
 
 	}
-	static void printTrie(DLBnode node, String prefix){
-		if (node.rightSib!=null){
-			printTrie()
+
+	static void printTrie(DLBnode node, String prefix) {
+		// System.out.println(node.child.val);
+
+		if (node.val == ('\'')) {
+			System.out.println(prefix);
+			return;
+		}
+		if (node.rightSib != null) {
+			printTrie(node.rightSib, prefix);
+		}
+		if (node.child != null) {
+			prefix += node.val;
+			printTrie(node.child, prefix);
 		}
 	}
 
@@ -63,45 +79,53 @@ public class pw_check {
 		if (empty) {
 
 			root = new DLBnode(word.charAt(0), null, null);
+			System.out.println("0: " + root.val);
+			root.child=new DLBnode(' ', null, null);
+			DLBnode currNode = root.child;
 			for (int i = 1; i < word.length(); ++i) {
-				root.child = new DLBnode(word.charAt(i), null, null);
-				root = root.child;
+
+				currNode = new DLBnode(word.charAt(i), null, null);
+				System.out.println(i + ": " + currNode.val);
+				currNode = currNode.child;
 			}
 
 			empty = false;
 			return;
-		}
-
-		DLBnode currentNode = root;
-		for (int i = 0; i < word.length();) {
-			boolean check = false; 
-			if (currentNode.rightSib != null) { 
-												
-				if (currentNode.val != word.charAt(i)) {
-					currentNode = currentNode.rightSib;
-					check = true;
-				} else { // CHECK COLUMN
-					++i;
-					currentNode = currentNode.child;
-					check = true;
-				}
+		} else {
+			if (root == null) {
+				System.out.println("yes"); 									// for testing purposes
 			}
-			if (currentNode.rightSib == null
-					&& currentNode.val == word.charAt(i) && !check) {
-				++i;
-				currentNode = currentNode.child;
-				check = true;
-			}
-			
-			if (!check) {
-				currentNode.rightSib = new DLBnode(word.charAt(i), null, null);
-				currentNode = currentNode.rightSib;
-				for (int a = i + 1; a < word.length(); ++a) {
-					currentNode.child = new DLBnode(word.charAt(a), null, null);
-					currentNode = currentNode.child;
+			DLBnode currentNode = root;
+			for (int i = 0; i < word.length();) {
+				//boolean check = false;
+				//System.out.println(word+"----------"+word.charAt(i));
+				//System.out.println("val: "+currentNode.val);
+				if (currentNode.val==word.charAt(i)){						//checks if char matches the node's val
+					currentNode=currentNode.child;
+					i++;
+				}else{
+					if (currentNode.rightSib==null){						// if char doesnt match val & rightSib doesnt exist
+						currentNode.rightSib=new DLBnode (word.charAt(i), null, null);
+						currentNode=currentNode.rightSib;
+						currentNode.child=new DLBnode(' ', null, null);
+						for (int a=i; a<word.length(); a++){
+							currentNode = new DLBnode(word.charAt(a), null, null);
+							System.out.println(a + ": " + currentNode.val);
+							if (word.charAt(a)!='\''){
+								currentNode.child=new DLBnode(' ', null, null);
+								currentNode = currentNode.child;
+								
+							}
+						}
+						break;
+					}else if (currentNode.rightSib!=null){
+																		//if rightSib exists then move currentNode to rightSib and repeat for loop
+						currentNode=currentNode.rightSib;
+					}
+					
 				}
-
-				break;
+				//System.out.println(i + ": " + currentNode.val);
+				
 			}
 		}
 	}
@@ -109,12 +133,11 @@ public class pw_check {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		DLBnode DLB = new DLBnode(' ', null, null);
-		/*if (args.length > 0) {
-			if (args[0].equals("-g")) {
-				buildTrie(DLB);
-			}
-		}*/
-		buildTrie(DLB); 
+		/*
+		 * if (args.length > 0) { if (args[0].equals("-g")) { buildTrie(DLB); }
+		 * }
+		 */
+		buildTrie(DLB);
 	}
 
 }
