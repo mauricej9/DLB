bb1b24de8455672816f85b711063117792d81b67
Maurice Jenkins <mauricejenkins9@gmail.com>
2015-10-01 21:30:04 -0400

    finished add method and started good password generation

--- pw_check.java
+++ pw_check.java
@@ -1,14 +1,17 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 public class pw_check {
 	static boolean empty = true;
-	private static DLBnode root;
+	private static DLBnode root = new DLBnode(' ', null, null);
 	static ArrayList<String> goodPass = new ArrayList<String>();
 	static ArrayList<String> badPass = new ArrayList<String>();
 
@@ -35,21 +38,21 @@ public class pw_check {
 		// BufferedReader read = new BufferedReader(new FileReader(
 		// "C:/Users/maurice/cs1501/DLB/src/dictionary.txt"));
 
-		//DLBnode rootNode = root;
+		// DLBnode rootNode = root;
 		BufferedReader read = new BufferedReader(new FileReader(
-				"C:/Users/maurice/cs1501/DLB/src/small_dict.txt")); // for
+				"C:/Users/maurice/cs1501/DLB/src/dictionary.txt")); // for
 																	// testing
 																	// purposes
 		String s;
-		File file = new File(
+		/*File file = new File(
 				"C:/Users/maurice/cs1501/DLB/src/my_dictionary.txt");
 		file.createNewFile();
-		FileWriter writer = new FileWriter(file);
+		FileWriter writer = new FileWriter(file);*/
 		while ((s = read.readLine()) != null) {
-			if (s.length() < 6) {
-				writer.write(s + "\n");
-				writer.write(System.getProperty("line.separator"));
-				//badPass.add(s);
+			if (s.length() < 5&&s.length()>0) {
+//				writer.write(s + "\n");
+//				writer.write(System.getProperty("line.separator"));
+				// badPass.add(s);
 				s += "'"; // the character ' signifies the end of the string in
 							// the trie
 				add(s);
@@ -58,8 +61,8 @@ public class pw_check {
 			}
 		}
 		// writer.flush();
-		writer.close();
-		read.close();
+		//writer.close();
+		//read.close();
 		// printTrie(rootNode, "");
 
 	}
@@ -75,11 +78,15 @@ public class pw_check {
 	 */
 
 	static boolean search(String s) {
-		DLBnode node = root;
+		DLBnode node = root.child;
 		// boolean found=false;
 		for (int i = 0; i < s.length();) {
-			// System.out.println("node val: " + node.val);
-			// System.out.println("char: " + s.charAt(i));
+			//System.out.println("node val: " + node.val);
+			if (node.rightSib == null) {
+				//System.out.println("no right sib");
+			}
+			//System.out.println("char: " + s.charAt(i));
+
 			if (node.val == s.charAt(i)) {
 				node = node.child;
 				i++;
@@ -132,7 +139,7 @@ public class pw_check {
 				word += s.charAt(j);
 			}
 		}
-		//System.out.println("word: " + word);
+		// System.out.println("word: " + word);
 		/*
 		 * File file = new File(
 		 * "C:/Users/maurice/cs1501/DLB/src/my_dictionary.txt"); if
@@ -141,17 +148,6 @@ public class pw_check {
 		 * BufferedWriter(fw); bw.write(word); bw.close();
 		 */
 		if (!badPass.contains(word)) {
-			try {
-				String filename = "C:/Users/maurice/cs1501/DLB/src/my_dictionary.txt";
-				FileWriter fw = new FileWriter(filename, true); // the true will
-																// append the
-																// new data
-				fw.write(word);// appends the string to the file
-				fw.write(System.getProperty("line.separator"));
-				fw.close();
-			} catch (IOException ioe) {
-				System.err.println("IOException: " + ioe.getMessage());
-			}
 			badPass.add(word);
 			add(word);
 		}
@@ -159,10 +155,41 @@ public class pw_check {
 	}
 
 	static void add(String word) {
-		DLBnode node =root;
-		for ( int i=0; i< word.length();){
-			if (node.child==null){
+		//System.out.println("added word: " + word);
+		DLBnode node = root;
+		for (int i = 0; i < word.length();i++) {
+			if (node.child != null) {
+				node = node.child;
+			} else {
+				node.child = new DLBnode(word.charAt(i), null, null);
 				
+				node = node.child;
+			}
+			while (node.val != word.charAt(i)) {
+
+				if (node.rightSib != null) {
+					node = node.rightSib;
+				} else {
+					node.rightSib = new DLBnode(word.charAt(i), null, null);
+					node=node.rightSib;
+				}
+			}
+		}
+	}
+	
+	static void generateGoodPasswords(){
+		char[] chars= {'b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','!','@','$','%','&','*'};
+		for (int a=0; a<chars.length; a++){
+			for (int b=0; b<chars.length; b++){
+				for (int c=0; c<chars.length; c++){
+					for (int d=0; d<chars.length; d++){
+						for (int e=0; e<chars.length; e++){
+							char[] pass={chars[a],chars[b],chars[c],chars[d],chars[e]};
+							System.out.println(Character.toString(pass[0])+Character.toString(pass[1])+Character.toString(pass[2])+Character.toString(pass[3])+Character.toString(pass[4]));
+							String password=pass.toString();
+						}
+					}
+				}
 			}
 		}
 	}
@@ -170,16 +197,25 @@ public class pw_check {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		DLBnode DLB = new DLBnode(' ', null, null);
-		/*
-		 * if (args.length > 0) { if (args[0].equals("-g")) { buildTrie(DLB); }
-		 * }
-		 */
-		// leetAdd("the");
 		buildTrie(DLB);
-		for (int i=0; i<badPass.size(); i++){
-			//System.out.println(badPass.get(i));
-		}
-		System.out.println(search("them"));
+		
+		 try {
+	            //Whatever the file path is.
+	            File statText = new File("C:/Users/maurice/cs1501/DLB/src/my_dictionary.txt");
+	            FileOutputStream is = new FileOutputStream(statText);
+	            OutputStreamWriter osw = new OutputStreamWriter(is);    
+	            Writer w = new BufferedWriter(osw);
+	            for (int i = 0; i < badPass.size(); i++) {
+	    			//System.out.println(badPass.get(i));
+	    			w.write(badPass.get(i));
+	    			w.write(System.getProperty("line.separator"));
+	    		}
+	            w.close();
+	        } catch (IOException e) {
+	            System.err.println("Problem writing to the file statsTest.txt");
+	        }
+		 generateGoodPasswords();
+		
 	}
 
 }
